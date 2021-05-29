5db2b4f0131e22e019aebe26736a9057f98024f3
Andrew Risse <andrewrisse@gmail.com>
2021-05-28 20:05:09 -0600

    Add types for req/res

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -23,7 +23,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  app.get("/filteredImage", async (req, res) => {
+  app.get("/filteredImage", async (req: Request, res: Response) => {
       const image_url = req.query.image_url;
       if(!image_url){
         return res.status(400).send("image_url query param required")
@@ -40,7 +40,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     
     res.send("try GET /filteredimage?image_url={{}}")
   } );
